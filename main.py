a0189af7c94621b5707a2e243e795a3019571137
ne0ascorbinka <ne.ascorbinka@gmail.com>
2024-09-01 15:34:55 +0300

    added CRUD commands parsers

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# import argparse
 from argparse import ArgumentParser
 from typing import Optional
 
 class Args:
     command: str
     description: Optional[str]
+    id: Optional[int]
 
 
 def main() -> None:
@@ -15,10 +15,18 @@ def main() -> None:
     add_parser = subparsers.add_parser('add', help="Add a new task")
     add_parser.add_argument("description", type=str, help="A short description of the task")
 
+    update_parser = subparsers.add_parser('update', help="Update task description by ID")
+    update_parser.add_argument('id', type=int,help="Task ID")
+
+    delete_parser = subparsers.add_parser('delete', help="Delete task by ID")
+    delete_parser.add_argument('id', type=int, help="Task ID")
+
     args: Args = parser.parse_args()
 
     if args.command == "add":
         print(f"Task '{args.description}' added successfully (ID: 1)")
+    elif args.command == "update":
+        print(f"Task updated successfully (ID: {args.id})")
     else:
         print("Unknown command. Use --help arg to seek for help (or go to therapy)")
     
