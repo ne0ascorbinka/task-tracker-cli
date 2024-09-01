8b618884e57a4e5c251f915b7fee5aadfb4b616a
ne0ascorbinka <ne.ascorbinka@gmail.com>
2024-09-01 18:18:08 +0300

    fixed update arguments and minor changes

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 from argparse import ArgumentParser
-from typing import Optional, Literal
-
-Status = Literal['done', 'todo', 'in-progress']
-
-class Args:
-    command: str
-    description: Optional[str]
-    id: Optional[int]
-    status: Optional[Literal['done', 'todo', 'in-progress']]
-
+from utils import Args
+from storage import Storage
+from command_processor import CommandProcessor
 
 def main() -> None:
     parser = ArgumentParser(prog='task-cli', description="Task tracker CLI instrument")
 
-    subparsers = parser.add_subparsers(dest='command', help="Basic commands")
+    subparsers = parser.add_subparsers(dest='command', help="Commands")
 
     add_parser = subparsers.add_parser('add', help="Add a new task")
     add_parser.add_argument("description", type=str, help="A short description of the task")
 
     update_parser = subparsers.add_parser('update', help="Update task description by ID")
     update_parser.add_argument('id', type=int,help="Task ID")
+    update_parser.add_argument('description', type=str, help="New task description")
 
     delete_parser = subparsers.add_parser('delete', help="Delete task by ID")
     delete_parser.add_argument('id', type=int, help="Task ID")
@@ -31,19 +25,21 @@ def main() -> None:
     mark_done_parser.add_argument('id', type=int, help="Task ID")
 
     list_parser = subparsers.add_parser('list', help="List tasks")
-    list_parser.add_argument('status', nargs='?', choices=['done', 'todo', 'in-progress'])
+    list_parser.add_argument('status', nargs='?', choices=['done', 'todo', 'in-progress'],
+                             help="Filter tasks by status [done | todo | in-progress]")
 
     args: Args = parser.parse_args()
 
-    if args.command == "add":
-        print(f"Task '{args.description}' added successfully (ID: 1)")
-    elif args.command == "update":
-        print(f"Task updated successfully (ID: {args.id})")
-    elif args.command == "list":
-        print(f"Listing task by {args.status}: 1, 2, 4")
-    else:
-        print("Unknown command. Use --help arg to seek for help (or go to therapy)")
-    
+    try:
+        storage = Storage()
+        command_processor = CommandProcessor(storage)
+
+        print(command_processor.process(args))
+    except Exception as e:
+        print("Error 500!\n\n")
+        print(e.with_traceback())
+
+        
 
 if __name__ == '__main__':
     main()
