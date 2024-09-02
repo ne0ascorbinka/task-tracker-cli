c2f2603e92c8b80e2f18610ef6d7c3aee640a998
ne0ascorbinka <ne.ascorbinka@gmail.com>
2024-09-03 00:14:19 +0300

    setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="task-cli",
-    version="0.1",
+    version="1.0",
     py_modules=["main", "utils", "command_processor", "storage"],
     entry_points={
         'console_scripts' : [
